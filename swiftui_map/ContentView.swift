7f09194ee6619bcab374d490b20465811762d39a
George  Ikwegbu Chinedu <g.ikwegbu@gmail.com>
2024-08-02 10:01:35 +0100

    Initial Commit

---
+++ swiftui_map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui_map
+//
+//  Created by gikwegbu on 02/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_map/swiftui_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mapApp.swift
+//  swiftui_map
+//
+//  Created by gikwegbu on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
